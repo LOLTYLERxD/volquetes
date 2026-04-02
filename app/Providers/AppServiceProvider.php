b946b052d75b37ae6def32becd1fdb409132ee1b
Kevin <kevinpage866@gmail.com>
2026-04-02 02:01:32 -0300

    fix: forzar HTTPS y variables DB

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,8 +20,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        Vite::prefetch(concurrency: 3);
+public function boot(): void
+{
+    if ($this->app->environment('production')) {
+        URL::forceScheme('https');
     }
+
+    Vite::prefetch(concurrency: 3);
+}
 }
